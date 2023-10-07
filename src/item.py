dff33355f90fd778c2d4bb2f9ba2dc601f5a2239
tatyana lozovaya <tatka8632@gmail.com>
2023-10-07 20:05:46 +0300

    реализация методов, тестирование

--- src/item.py
+++ src/item.py
@@ -3,6 +3,7 @@ class Item:
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
+    discount = 0.85 # скидка 15%
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
@@ -13,18 +14,20 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
-    def calculate_total_price(self) -> float:
+    def calculate_total_price(self) -> str:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= Item.discount
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+def test_calculate_total_price():
+    item = Item('Смартфон', 1.0, 10)
+    assert item.calculate_total_price() == 10.0
+
+def test_apply_discount():
+    item = Item('Смартфон', 1.0, 10)
+    item.apply_discount()
+    assert item.price == 0.85
