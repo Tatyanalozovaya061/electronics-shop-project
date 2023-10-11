4316dcbe76c50e704fb02967251cb9ef11aa04f7
tatyana lozovaya <tatka8632@gmail.com>
2023-10-11 19:36:05 +0300

    классметод, статметод, тесты

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import os
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -19,10 +22,36 @@ class Item:
         self.quantity = quantity
         Item.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, csvfile='src/items.csv'):
+        cls.all.clear()
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        root_dir = os.path.split(current_dir)[0]
+        filepath = os.path.join(root_dir, csvfile)
+        with open(filepath, 'r', encoding='windows-1251') as f:
+            rows = csv.DictReader(f)
+            for row in rows:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name=name, price=price, quantity=quantity)
+
+    @staticmethod
+    def string_to_number(num):
+        return int(float(num))
+
+
     def calculate_total_price(self) -> str:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
--- tests/test_item.py
+++ tests/test_item.py
@@ -10,3 +10,25 @@ def test_apply_discount():
     item = Item('Смартфон', 1.0, 10)
     item.apply_discount()
     assert item.price == 0.85
+
+def test_name():
+    item = Item('Телефон', 10000, 5)
+
+    # длина наименования товара меньше 10 символов
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+    # длина наименования товара больше 10 символов
+    item.name = 'СуперСмартфон'
+    assert item.name == 'СуперСмарт'
+
+def test_instantiate():
+    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+def test_convert():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
